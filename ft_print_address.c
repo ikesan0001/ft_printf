26b0a2b63bb164f7797d75f05cf00ab02347dae1
iryoga <iryoga@student.42tokyo.jp>
2022-08-18 07:00:36 +0900

    test

--- ft_print_address.c
+++ ft_print_address.c
@@ -6,14 +6,33 @@
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 23:49:16 by iryoga            #+#    #+#             */
-/*   Updated: 2022/08/18 04:50:52 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/08/18 07:00:12 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/ft_printf.h"
 
-int	ft_print_address(unsigned long n)
+static int ft_addresslen(uintptr_t ptr);
+
+int	ft_print_address(uintptr_t ptr)
 {
-	n++;
+	int	len;
+
+	len = 0;
+	len += ft_print_str("0x");
+	len += ft_print_hex(ptr, 'x');
 	return (0);
+}
+
+static int ft_addresslen(uintptr_t ptr)
+{
+	int	len;
+
+	len = 0;
+	while (ptr != 0)
+	{
+		ptr /= 16;
+		len++;
+	}
+	return (len);
 }
--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 06:05:22 by iryoga            #+#    #+#             */
-/*   Updated: 2022/08/18 03:40:31 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/08/18 07:00:31 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <stdarg.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdint.h>
 
 # include <stdio.h>
 # include <limits.h>
@@ -24,7 +25,7 @@ int		ft_printf(const char *fmt, ...);
 
 int		ft_print_char(int c);
 int		ft_print_str(char *s);
-int		ft_print_address(unsigned long n);
+int		ft_print_address(uintptr_t ptr);
 int		ft_print_nbr(int n);
 int		ft_print_unnbr(unsigned int n);
 int		ft_print_hex(unsigned int n, int specifier);
