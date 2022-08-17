b5670af3dcc7824b92abdd7b1b3e03fcd1e740c6
iryoga <iryoga@student.42tokyo.jp>
2022-08-18 05:16:24 +0900

    スペルミス

--- ft_print_str.c
+++ ft_print_str.c
@@ -6,18 +6,18 @@
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 23:49:27 by iryoga            #+#    #+#             */
-/*   Updated: 2022/08/18 05:07:51 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/08/18 05:16:08 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/ft_printf.h"
 
-int	ft_printstr(char *s)
+int	ft_print_str(char *s)
 {
 	int	len;
 
 	if (s == NULL)
-		return (ft_printstr("(null)"));
+		return (ft_print_str("(null)"));
 	len = 0;
 	while (s[len] != '\0')
 		len++;
