a4238f85eb4c238032dd8fd07a4779c9eae573a4
iryoga <iryoga@student.42tokyo.jp>
2022-08-18 07:35:02 +0900

    return_value

--- ft_print_address.c
+++ ft_print_address.c
@@ -6,7 +6,7 @@
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 23:49:16 by iryoga            #+#    #+#             */
-/*   Updated: 2022/08/18 07:01:37 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/08/18 07:34:50 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_print_address(uintptr_t ptr)
 	len = 0;
 	len += ft_print_str("0x");
 	len += ft_print_hex(ptr, 'x');
-	return (0);
+	return (len);
 }
 
 /*
