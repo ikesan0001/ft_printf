0a48cb876e016bb34904c12d0dcf51606ec704f5
iryoga <iryoga@student.42tokyo.jp>
2022-08-18 07:01:44 +0900

    test

--- ft_print_address.c
+++ ft_print_address.c
@@ -6,13 +6,13 @@
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 23:49:16 by iryoga            #+#    #+#             */
-/*   Updated: 2022/08/18 07:00:12 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/08/18 07:01:37 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/ft_printf.h"
 
-static int ft_addresslen(uintptr_t ptr);
+//static int ft_addresslen(uintptr_t ptr);
 
 int	ft_print_address(uintptr_t ptr)
 {
@@ -24,6 +24,7 @@ int	ft_print_address(uintptr_t ptr)
 	return (0);
 }
 
+/*
 static int ft_addresslen(uintptr_t ptr)
 {
 	int	len;
@@ -36,3 +37,4 @@ static int ft_addresslen(uintptr_t ptr)
 	}
 	return (len);
 }
+*/
