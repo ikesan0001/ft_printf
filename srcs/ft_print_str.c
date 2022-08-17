6c8b386562769ee385dfa728395f7cd2b9ff7752
iryoga <iryoga@student.42tokyo.jp>
2022-08-18 00:24:08 +0900

    touch some files

--- srcs/ft_print_str.c
+++ srcs/ft_print_str.c
@@ -1,32 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_print_str.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/11 06:05:22 by iryoga            #+#    #+#             */
-/*   Updated: 2022/08/17 23:49:06 by iryoga           ###   ########.fr       */
+/*   Created: 2022/08/17 23:49:27 by iryoga            #+#    #+#             */
+/*   Updated: 2022/08/18 00:24:08 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
-
-# include <stdarg.h>
-# include <unistd.h>
-# include <stdlib.h>
-
-# include <stdio.h>
-# include <limits.h>
-
-int		ft_printf(const char *fmt, ...);
-
-int		ft_print_char(int c);
-int		ft_print_str(char *s);
-int		ft_print_address(unsigned long n);
-int		ft_print_nbr(int n);
-int		ft_print_unnbr(unsigned int n);
-int		ft_print_hex(unsigned int n);
-
-#endif
--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,19 +6,35 @@
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 06:11:18 by iryoga            #+#    #+#             */
-/*   Updated: 2022/08/16 03:49:58 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/08/18 00:24:08 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		ft_printf(const char *fmt, ...)
+static int	ft_check_types(va_list ap);
+
+int	ft_printf(const char *fmt, ...)
 {
 	va_list	ap;
 	int		length;
+	int		i;
 
+	length = 0;
+	i = 0;
+	if (fmt == NULL)
+		return (-1);
 	va_start(ap, fmt);
-	
+	while (fmt[i] != '\0' && length < INT_MAX)
+	{
+		if (fmt[i] == '%')
+		{
+			i++;
+			length += ft_check_types();
+		}
+		length += ft_printchar();
+		i++;
+	}
 	va_end(ap);
-	return (0);
+	return (length);
 }
